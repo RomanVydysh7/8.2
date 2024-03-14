51f4e74b8d350ccfac43d7dc9db902f3b5eccd8b
Roman 111 <romanvydysh7@gmail.com>
2024-03-14 15:16:17 +0200

    update

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype> 
+
+
+int findLongestWordLength(const std::string& str) {
+    int maxLength = 0; 
+    int currentLength = 0; 
+
+    for (char c : str) {
+        if (std::isalpha(c)) { 
+            currentLength++;
+        }
+        else {
+            if (currentLength > maxLength) {
+                maxLength = currentLength; 
+            }
+            currentLength = 0; 
+        }
+    }
+
+ 
+    if (currentLength > maxLength) {
+        maxLength = currentLength;
+    }
+
+    return maxLength;
+}
+
+int main() {
+    std::string str;
+
+    std::cout << "Input: ";
+    std::getline(std::cin, str);
+
+    int longestWordLength = findLongestWordLength(str);
+    std::cout << "max: " << longestWordLength << std::endl;
+
+    return 0;
+}
